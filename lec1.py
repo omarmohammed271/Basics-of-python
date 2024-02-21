595376ff4ace3c5eec1c2de2d7fa867dbb8fb5ac
moooooora <omarmohammed271@gmail.com>
2024-02-21 19:50:04 +0200

    Upload our first project

---
+++ lec1.py
@@ -0,0 +1,31 @@
+# print('Hello Moustafa')
+# print("hello")
+# print(55)
+# print(55.544646)
+# Comment
+# make sure run application
+
+# print('Hello Omar')
+# # print('Hello ' + 'Omar')
+# print('Hello' , 'Omar')
+# print('Hello' ,5)
+print('Hello',input('Enter your name:'),'your age is',input('Enter your age'))
+# print('Hello',input('Enter your name:'),'your age is',input('Enter your age'))
+
+
+# () **  */  +-
+# 
+# Omar
+# Mohamed
+# Moustafa 
+# Rehab
+# Heba 
+# Abdelrahman
+
+# Github
+# gitlab
+
+
+# git ----> laptop to server
+# server (Github)
+# social website ---> linkedin
