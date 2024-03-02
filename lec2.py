4547197bbc5fd3b2a412492254d6cbcab9acca45
moooooora <omarmohammed271@gmail.com>
2024-03-02 21:03:53 +0200

    Solving Task 1

---
+++ lec2.py
@@ -0,0 +1,153 @@
+# print()---->output
+# input() ---> input
+#    comment
+# variables ----->  
+# name1 = 'Ahmed'
+# name2 = 'mohamed'
+# number = 10
+
+# first_name = input('Enter your first name:')
+# last_name = input('Enter your last name:')
+
+# full_name = first_name + last_name
+# print('Welcome',full_name)
+# num1=5
+# num2=10
+# result = num1 + num2
+# print(result)
+
+# name = 'Omar'
+# number = 55
+# print(type(name))
+# print(type(number))
+# Data Types
+# string ---> str ----> words
+# integer --> int ----> real number
+# float ----> float ---> digits number 5.5 0.165464
+# Boolean ---> bool ----> True ,False
+# List ---> list -----> ['Omar',10,0.64654,True,['OMar','AHmed'],()]
+# tuple ----> tuple ----> ('sat','mon',10,.16516,(),[])
+#DIctionary ---> dict----> {'key':'value'}
+
+# name = "Heba" 
+# print(type(name))
+# number = 10
+# print(type(number))
+# number = 10.151651
+# print(type(number))
+# x = True
+# print(type(x))
+# employee = ['Omar',28,'Data scientist ']
+# employee = ('Omar',28,'Data scientist ')
+# print(employee)
+# print(type(list))
+# name = 'Omar'
+# age = 28
+# job_title = 'Backend Engineer'
+# employee = {
+#     'name' : 'Omar',
+#     'age' : 28,
+#     'Job title':'Backend Engineer',
+# }
+# print(employee)
+
+# Operators
+# + - * / // ** %
+num1 = 20
+num2 = 6
+# result = num1+num2
+# result = num1-num2
+# result = num1*num2
+# result = num1/num2
+# result = num1//num2
+# result = num1%num2 20/100
+# result = num1**num2 ---> power
+# print(result)
+
+# print(10%2)
+
+
+# # > < ==  >= <= !=
+# num1 = 7
+# num2 = 5
+# print(num1>num2)
+# print(num1<num2)
+# print(num1==num2)
+# print(num1>=num2)
+# print(num1<=num2)
+# print(num1!=num2)
+
+# if condition:
+#     coding
+# else:
+#     coding    
+
+# num1 = 5
+# num2 = 5
+
+# if num1 > num2 :
+#     print('num1 > num2')
+# elif num1 < num2 :
+#     print('num1<num2')  
+# else:
+#     print('numbers are equals ')    
+# age = int(input('Enter your age:'))
+# print(age)
+# print(type(age))
+# >90----->Excellent
+# 90>result>80 ===> Very Good
+# 80>result>70 ===> Good
+# 70>result>60 ===> pass
+# result<60 -----> Failed
+
+# grade = int(input('pls Enter your grade:'))
+
+# if grade >= 90 :
+#     print('Excllent')
+# if grade<90 and grade>=80:
+#     print('Very Good')    
+# if 80>grade>=70:
+#     print('Good') 
+# if 70>grade>=60:
+#     print('pass')
+# else:
+#     print('Failed') 
+        
+# grade = int(input('pls Enter your grade:'))
+
+# if grade >= 90 :
+#     print('Excllent')
+# elif grade<90 and grade>=80:
+#     print('Very Good')    
+# elif 80>grade>=70:
+#     print('Good') 
+# elif 70>grade>=60:
+#     print('pass')
+# else:
+#     print('Failed')      
+
+
+# num1 = 5
+# num2 = 20
+
+# if num1<num2 or num1!=5:
+#     print(True)
+# else:
+#     print(False)    
+# if num1<num2 and num1!=5:
+#     print(True)
+# else:
+#     print(False)    
+
+
+# short if
+x = 10
+y = 5
+print(True) if x>y else print(False)
+
+if x>y:
+    print(True)
+else:
+    print(False)    
+
+    # How to write clean code
---
+++ task1.py
@@ -0,0 +1,18 @@
+# Q1
+# x = 10
+# y = 5
+# print(True) if x>y else print(False)
+
+# if x>y:
+#     print(True)
+# else:
+#     print(False)    
+# Q2)
+x = 10
+y = 5
+print(True) if x>y else print(False)
+
+if x>y:
+    print(True)
+else:
+    print(False)    
